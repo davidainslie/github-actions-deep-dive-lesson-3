07c61980ea7b36eb43f186042877a4d8a88bb767
David Ainslie <dainslie@gmail.com>
2022-04-30 11:28:58 +0100

    GitHub Pages with pandoc

--- terraform/output.tf
+++ terraform/output.tf
@@ -1,3 +1,8 @@
+output "account-id" {
+  # Careful - Cheating here to view sensitive data.
+  value = nonsensitive(var.account-id)
+}
+
 output "github-actions-user-access-key" {
   value = aws_iam_access_key.github-actions-user-access.id
 }
@@ -5,4 +10,9 @@ output "github-actions-user-access-key" {
 output "github-actions-user-secret-key" {
   # Careful - Cheating here to view sensitive data.
   value = nonsensitive(aws_iam_access_key.github-actions-user-access.secret)
+}
+
+output "bucket" {
+  # Careful - Cheating here to view sensitive data.
+  value = var.bucket
 }
